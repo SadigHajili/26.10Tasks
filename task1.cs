04b116676ce80c91b630d5555cf8e66edb64a396
Sadig Hajili <92145426+SadigHajili@users.noreply.github.com>
2021-10-26 00:58:44 +0400

    home task

---
+++ task1.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+    static void Main() {
+          int a=100,i=1; 
+    while (i<100) {
+    Console.WriteLine(i);
+    i++;   
+    }
+  }
+}
+  
---
+++ task3.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+    static void Main() {
+     int i;
+     string a;
+     a = Console.ReadLine();
+     i = int.Parse(a);
+     Console.WriteLine(i*2);
+        
+    }
+}
